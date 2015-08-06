01e4bf10cd82696a8ded05ddf4ae0939323a7e8b
asharijuang <asharijuang@me.com>
2015-08-06 08:43:14 +0700

    Initial Commit

---
+++ UIView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIView
+//
+//  Created by Ashari Juang on 8/6/15.
+//  Copyright (c) 2015 KodeJS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIView
+//
+//  Created by Ashari Juang on 8/6/15.
+//  Copyright (c) 2015 KodeJS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
